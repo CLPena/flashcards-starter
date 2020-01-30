5e44364f2d85a0c99e70586dc013cf8dcd03c09b
Cristina Pena <cristinalpena@gmail.com>
2020-01-30 15:21:58 -0700

    Add two tests to game

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,11 +2,27 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 
 describe('Game', function() {
 
   it('should be a function', function() {
     expect(Game).to.be.a('function');
+  });
+
+  it('should create a new instance of Deck', function() {
+    const game = new Game();
+    game.start();
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should create a new instance of Round', function() {
+    const game = new Game();
+    game.start();
+    expect(Round).to.be.a('function');
   });
 
   it('should be an instance of Game', function() {
@@ -19,7 +35,11 @@ describe('Game', function() {
     game.start();
     expect(game.round).to.be.an('object');
   });
-});
 
-// test that deck and round are created
-// test for length of cards array
+  it('should hold all the cards in the cards array', function() {
+    const game = new Game();
+    game.start();
+    expect(game.cards.length).to.equal(prototypeQuestions.length);
+  });
+
+});
